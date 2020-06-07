0519fe1e0d6dcb844992e0fe195208d9fccad01d
Denis <you@example.com>
2020-06-07 16:34:02 +0300

    Basics.

--- STUDY.NET.WEB/STUDY.NET.WEB.MVC/Startup.cs
+++ STUDY.NET.WEB/STUDY.NET.WEB.MVC/Startup.cs
@@ -5,36 +5,56 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace STUDY.NET.WEB.MVC
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<FeatureToggles>(i => new FeatureToggles
+            {
+                DeveloperExceptions = configuration
+                .GetValue<bool>("FeatureToggles:EnableDeveloperExceptions")
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(
+            IApplicationBuilder app, 
+            IWebHostEnvironment env,
+            FeatureToggles feature)
         {
-            if (env.IsDevelopment())
+            app.UseExceptionHandler("/error.html");
+
+            if (configuration.GetValue<bool>("FeatureToggles:EnableDeveloperExceptions"))
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            app.Use(async (context, next) =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                if (context.Request.Path.Value.Contains("invalid"))
+                    throw new ArgumentException("Invalid mistake!");
+
+                await next();
             });
+
+            app.UseFileServer();
         }
     }
 }
